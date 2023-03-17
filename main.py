5b0603193fe8322682508add9d6ded8be8fadd99
oshmetochki <toni.krivorot@gmail.com>
2023-03-18 00:56:56 +0300

    new

--- main.py
+++ main.py
@@ -6,24 +6,29 @@ from token import token
 API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = token
 TEXT: str = 'ggs'
+API_CATS_URL: str = 'https://aws.random.cat/meow'
 MAX_COUNTER: int = 10
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
 
 offset: int = -2
 counter: int = 0
 chat_id: int
 
 
-while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
+while counter < 100:
+    print('attempt =', counter)
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()['file']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
